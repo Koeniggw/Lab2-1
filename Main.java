d49d92d2e28aaf87f75c0dd787104fef696fd6ff
Graham Koenig <89868570+Koeniggw@users.noreply.github.com>
2021-09-14 20:12:39 +0000

    Minor wording changes

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Enter a number to count down or up from: ");
+    System.out.println("Type a number to count down or up from then press \"Enter\": ");
 
     Scanner num = new Scanner(System.in);
 
